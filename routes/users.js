7966cb04ce65dd4f3a646b029c6210f11c310482
Nikos Ntaf <nik.ntf1@gmail.com>
2018-12-19 18:20:48 +0200

    authentication

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const errors = require('restify-errors');
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+const auth = require('../auth');
+const jwt = require('jsonwebtoken');
+const config = require('../config');
 
 module.exports = server => {
     //Register User
@@ -26,4 +29,23 @@ module.exports = server => {
             });
         })
     });
-}
+    //Authenticate User
+    server.post('/auth', async (req,res,next)=> {
+        const {email,password} = req.body
+        try{
+            //authenticate user
+            const user =await auth.authenticate(email,password);
+            //create JWT
+            const token = jwt.sign(user.toJSON(),config.JWT_SECRET, {
+                expiresIn: '15m'
+            });
+            const { iat,exp } = jwt.decode(token);
+            //respond with token
+            res.send({iat,exp,token});            
+            next();
+        }catch(error){
+            //user unauthorized
+            return next(new errors.UnauthorizedError(error));
+        }
+    });
+};
